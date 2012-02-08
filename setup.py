239627bb91e4e6b22d94c930efb7bfc3c1d46f20
Dzhelil Rufat <dzhelil@gmail.com>
2012-02-07 22:26:14 -0800

    Prepare for pypi

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ import numpy
 from distutils.core import setup, Extension
 
 setup(name='triangle',
-      version='0.1',
-      requires = ['numpy'],
+      packages=['triangle'],
+      version='0.3',
       description='Python binding to the triangle library',
       author='Dzhelil Rufat',
       author_email='drufat@caltech.edu',
-      packages=['triangle'],
+      url='https://github.com/drufat/triangle',
+      requires = ['numpy'],
       ext_modules=[Extension('triangle.core', ['c/triangle.c', 'c/triangle_ext.c'],
-                             include_dirs = (numpy.get_include(), 'c'),
+                             include_dirs = ['c', numpy.get_include()],
                              define_macros = [('NO_TIMER', 1), ('TRILIBRARY', 1)]) 
-              ],
-     )
+              ]
+      )
