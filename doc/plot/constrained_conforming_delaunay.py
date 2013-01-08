6b2adff758955e61a34b20b4222ee302f436cbe9
Dzhelil Rufat <drufat@caltech.edu>
2013-01-08 00:18:26 -0800

    Fix constrained delaunay plot.

--- doc/plot/constrained_conforming_delaunay.py
+++ doc/plot/constrained_conforming_delaunay.py
@@ -3,6 +3,6 @@ import triangle.plot as plot
 import matplotlib.pyplot as plt
 
 A = triangle.get_data('A')
-t = triangle.triangulate(A, 'pq0')
+t = triangle.triangulate(A, 'pq30')
 plot.plot(plt.axes(), **t)
 plt.show()
