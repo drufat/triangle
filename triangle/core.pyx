71b4f12198f0d85d7347ca72dbe17431b6ee0017
Dzhelil Rufat <drufat@caltech.edu>
2014-06-08 18:47:57 +0300

    Port to python version 3

--- triangle/core.pyx
+++ triangle/core.pyx
@@ -7,7 +7,7 @@ cdef replace_d(double** pA, double[:] B):
     if pA[0]: free(pA[0])
     # Allocate new memory
     pA[0] = <double*>malloc(sizeof(double)*B.size)
-    # Copy array in B to pA 
+    # Copy array in B to pA
     memcpy(pA[0], &(B[0]), sizeof(double)*B.size)
 
 cdef replace_i(int** pA, int[:] B):
@@ -17,9 +17,9 @@ cdef replace_i(int** pA, int[:] B):
 
 cdef copy_d(double** pA, int N):
     # Allocate new memory
-    cdef double* t = pA[0] 
+    cdef double* t = pA[0]
     pA[0] = <double*>malloc(sizeof(double)*N)
-    # Copy array 
+    # Copy array
     memcpy(pA[0], t, sizeof(double)*N)
 
 cdef cleanup(void** pA):
@@ -27,23 +27,23 @@ cdef cleanup(void** pA):
     pA[0] = NULL
 
 cdef class TriangulateIO:
-    
+
     cdef ct.triangulateio c
-    
+
     property pointlist:
         def __get__(self):
             return <double[:self.c.numberofpoints*2]> self.c.pointlist
         def __set__(self, double[:] value):
             replace_d(&(self.c.pointlist), value)
-            self.c.numberofpoints = value.size / 2            
-    
+            self.c.numberofpoints = value.size / 2
+
     property pointattributelist:
         def __get__(self):
             return <double[:self.c.numberofpointattributes]> self.c.pointattributelist
         def __set__(self, double[:] value):
             assert value.size == self.c.numberofpoints
             replace_d(&(self.c.pointattributelist), value)
-    
+
     property pointmarkerlist:
         def __get__(self):
             return <int[:self.c.numberofpoints]> self.c.pointmarkerlist
@@ -58,7 +58,7 @@ cdef class TriangulateIO:
             replace_i(&(self.c.trianglelist), value)
             self.c.numberofcorners = 3
             self.c.numberoftriangles = value.size/self.c.numberofcorners
-    
+
     property triangleattributelist:
         def __get__(self):
             return <double[:self.numberoftriangles*self.c.numberoftriangleattributes]> self.c.triangleattributelist
@@ -84,13 +84,13 @@ cdef class TriangulateIO:
             return self.c.numberofcorners
         def __set__(self, int value):
             self.c.numberofcorners = value
-                    
+
     property numberoftriangleattributes:
         def __get__(self):
             return self.c.numberoftriangleattributes
         def __set__(self, int value):
             self.c.numberoftriangleattributes = value
-        
+
     property neighborlist:
         def __get__(self):
             return <double[:self.c.numberoftriangleattributes]> self.c.triangleattributelist
@@ -111,7 +111,7 @@ cdef class TriangulateIO:
         def __set__(self, int[:]  value):
             assert value.size == self.c.numberofsegments
             replace_i(&(self.c.segmentmarkerlist), value)
-            
+
     property holelist:
         def __get__(self):
             return <double[:self.c.numberofholes*2]> self.c.holelist
@@ -139,7 +139,7 @@ cdef class TriangulateIO:
         def __set__(self, int[:] value):
             assert self.c.numberofedges == value.size
             replace_i(&(self.c.edgelist), value)
-    
+
     property normlist:
         def __get__(self):
             return <double[:self.c.numberofedges*2]> self.c.normlist
@@ -161,49 +161,52 @@ cdef class TriangulateIO:
         self.c.numberoftriangles = 0
         self.c.numberofcorners = 0
         self.c.numberoftriangleattributes = 0
-        
+
         self.c.segmentlist = NULL
         self.c.segmentmarkerlist = NULL
         self.c.numberofsegments = 0
-        
+
         self.c.holelist = NULL
         self.c.numberofholes = 0
-        
+
         self.c.regionlist = NULL
         self.c.numberofregions = 0
-        
+
         self.c.edgelist = NULL
         self.c.edgemarkerlist = NULL
         self.c.normlist = NULL
         self.c.numberofedges = 0
-        
+
     def __dealloc__(self):
         cleanup(<void**>&(self.c.pointlist))
         cleanup(<void**>&(self.c.pointattributelist))
         cleanup(<void**>&(self.c.pointmarkerlist))
 
-        cleanup(<void**>&(self.c.trianglelist))      
-        cleanup(<void**>&(self.c.triangleattributelist))      
-        cleanup(<void**>&(self.c.trianglearealist))      
-        cleanup(<void**>&(self.c.neighborlist)) 
-        
-        cleanup(<void**>&(self.c.segmentlist))      
+        cleanup(<void**>&(self.c.trianglelist))
+        cleanup(<void**>&(self.c.triangleattributelist))
+        cleanup(<void**>&(self.c.trianglearealist))
+        cleanup(<void**>&(self.c.neighborlist))
+
+        cleanup(<void**>&(self.c.segmentlist))
         cleanup(<void**>&(self.c.segmentmarkerlist))
-        
+
         cleanup(<void**>&(self.c.holelist))
         cleanup(<void**>&(self.c.regionlist))
-        
-        cleanup(<void**>&(self.c.edgelist))      
-        cleanup(<void**>&(self.c.edgemarkerlist))      
+
+        cleanup(<void**>&(self.c.edgelist))
+        cleanup(<void**>&(self.c.edgemarkerlist))
         cleanup(<void**>&(self.c.normlist))
-        
-def triang(char* switch, TriangulateIO in_, TriangulateIO out_, vorout=None):
+
+def triang(switch, in_, out_, vorout=None):
+    __triang(switch.encode('utf-8'), in_, out_, vorout)
+
+def __triang(char* switch, TriangulateIO in_, TriangulateIO out_, vorout=None):
     cdef TriangulateIO vorout_
     if vorout:
         vorout_ = vorout
         ct.triangulate(switch, &in_.c, &out_.c, &(vorout_.c))
     else:
-        ct.triangulate(switch, &in_.c, &out_.c, NULL)    
+        ct.triangulate(switch, &in_.c, &out_.c, NULL)
     # Copy whole array to avoid freeing of non-allocated pointers
     copy_d(&(out_.c.holelist), out_.c.numberofholes*2)
     copy_d(&(out_.c.regionlist), out_.c.numberofregions*4)
