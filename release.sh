e2d76bf8e0874a7a401efdccd804bbdb2e930526
Dzhelil Rufat <drufat@caltech.edu>
2015-12-13 18:26:16 -0800

    Add taggint to release.sh

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 VERSION=`python -c "exec(open('triangle/version.py').read()); print(__version__)"`
 echo v$VERSION
 
-echo v$VERSION
+git tag v$VERSION
 git push --tags
 python setup.py sdist
 twine upload dist/*
