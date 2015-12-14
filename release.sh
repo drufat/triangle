f8023e3f6f32c5ec1800c5a87e4159444997d736
Dzhelil Rufat <drufat@caltech.edu>
2015-12-13 18:19:19 -0800

    Fix release.sh

--- release.sh
+++ release.sh
@@ -4,6 +4,6 @@ VERSION=`python -c "exec(open('triangle/version.py').read()); print(__version__)
 echo v$VERSION
 
 echo v$VERSION
-git push --version
+git push --tags
 python setup.py sdist
 twine upload dist/*
