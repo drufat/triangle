2df6b0ad3a62d6e510e779bd1d08a8d553d51d7b
Dzhelil Rufat <drufat@caltech.edu>
2015-12-13 17:31:25 -0800

    Formatting.

--- setup.py
+++ setup.py
@@ -1,43 +1,54 @@
 from setuptools import setup, Extension
 
-define_macros = [('VOID', 'int'),
-                 ('REAL', 'double'),
-                 ('NO_TIMER', 1),
-                 ('TRILIBRARY', 1),
-                 ('ANSI_DECLARATORS', 1)]
+define_macros = [
+    ('VOID', 'int'),
+    ('REAL', 'double'),
+    ('NO_TIMER', 1),
+    ('TRILIBRARY', 1),
+    ('ANSI_DECLARATORS', 1),
+]
 
 setup(name='triangle',
-    packages=['triangle'],
-    package_dir={'triangle':'triangle'},
-    package_data={'triangle':['data/*.node',
-                             'data/*.ele',
-                             'data/*.poly',
-                             'data/*.area',
-                             'data/*.edge',
-                             'data/*.neigh']},
-    version='2015.12.13',
-    description='Python binding to the triangle library',
-    author='Dzhelil Rufat',
-    author_email='drufat@caltech.edu',
-    license='GNU LGPL',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-    ],
-    url='http://dzhelil.info/triangle',
-    setup_requires=['setuptools>=18.0',
-                    'Cython>=0.18'],
-    install_requires=['numpy>=1.7.0',
-                      'Cython>=0.18'],
-    ext_modules=[
-                 Extension('triangle.core', ['c/triangle.c', 
-                                             'triangle/c_triangle.pxd', 
-                                             'triangle/core.pyx'],
-                           include_dirs = ['c'],
-                           define_macros = define_macros)
-    ]
-)
+      packages=['triangle'],
+      package_dir={'triangle': 'triangle'},
+      package_data={'triangle': [
+          'data/*.node',
+          'data/*.ele',
+          'data/*.poly',
+          'data/*.area',
+          'data/*.edge',
+          'data/*.neigh'
+      ]},
+      version='2015.12.13',
+      description='Python binding to the triangle library',
+      author='Dzhelil Rufat',
+      author_email='drufat@caltech.edu',
+      license='GNU LGPL',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      url='http://dzhelil.info/triangle',
+      setup_requires=[
+          'setuptools>=18.0',
+          'Cython>=0.18'
+      ],
+      install_requires=[
+          'numpy>=1.7.0',
+          'Cython>=0.18'
+      ],
+      ext_modules=[
+          Extension('triangle.core',
+                    [
+                        'c/triangle.c',
+                        'triangle/c_triangle.pxd',
+                        'triangle/core.pyx'
+                    ],
+                    include_dirs=['c'],
+                    define_macros=define_macros)
+      ]
+      )
